82472fed5413f78aceb051da5b5fb530fb6fbee4
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-09-24 16:04:13 +0300

    bad commit, extract does not work

--- QueueJava/src/Main.java
+++ QueueJava/src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Йг
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+
+        int value, r, bufSize;
+        System.out.print("Количество элементов в стеке: ");
+        bufSize = console.nextInt();
+        QueueCustom currentQueue = new QueueCustom(bufSize);
+
+        do {
+            System.out.print("Для добавления введите 1, для извлечения 0: ");
+            r = console.nextInt();
+            if (r == 1 || r == 0){
+                if (r == 1){
+                    System.out.print("Введите значение: ");
+                    currentQueue.insert(console.nextInt());
+                }
+                else {
+                    System.out.print("Значение равно: ");
+                    System.out.println(currentQueue.remove());
+                }
+            }
+            else {
+                System.out.println("Incorrect input!\\nДля добавление введите 1, для извлечения 0: \\n");
+                r = console.nextInt();
+            }
+        }while(true);
     }
 }
